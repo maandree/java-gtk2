da12b15ed05a0c36176ca9c1f94d094c495a0198
Mattias Andrée <maandree@operamail.com>
2013-03-09 12:30:32 +0100

    java part of GTK Window with constructor
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ src/javagtk2/GWindow.java
@@ -0,0 +1,43 @@
+/**
+ * java-gtk2 – GTK+ 2.0 bindings for Java
+ * 
+ * Copyright © 2013  Mattias Andrée (maandree@member.fsf.org)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package javagtk2;
+
+
+/**
+ * GTK Window class
+ * 
+ * @author  Mattias Andrée <a href="mailto:maandree@member.fsf.org">maandree@member.fsf.org</a>
+ */
+public class GWindow
+{
+    /**
+     * Constructor
+     */
+    public GWindow()
+    {
+	super(__new());
+    }
+    
+    /**
+     * Native constructor
+     */
+    private static native long __new();
+    
+}
+
